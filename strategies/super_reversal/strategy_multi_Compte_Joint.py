6b076c9d109a9d43c60d32810251c317d5f1497f
conseilbasal <conseilbasal@gmail.com>
2022-05-13 21:53:13 +0200

    Update strategy_multi_Compte_Joint.py

--- strategies/super_reversal/strategy_multi_Compte_Joint.py
+++ strategies/super_reversal/strategy_multi_Compte_Joint.py
@@ -34,7 +34,7 @@ params_coin = {
         "short_ema_window": 3,
         "long_ema_window": 130
     },
-    "AAVE/USD": {
+    "EGLD/USD": {
         "wallet_exposure": 0.1,
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
