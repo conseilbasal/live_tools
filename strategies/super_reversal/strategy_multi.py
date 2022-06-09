379385b35207958d7993082eb77459a7574fc7eb
conseilbasal <conseilbasal@gmail.com>
2022-06-09 16:38:17 +0200

    Update strategy_multi.py

--- strategies/super_reversal/strategy_multi.py
+++ strategies/super_reversal/strategy_multi.py
@@ -69,7 +69,7 @@ params_coin = {
         "short_ema_window": 3,
         "long_ema_window": 130
     },
-    "AVAX/USD": {
+    "SOL/USD": {
         "wallet_exposure": 0.1,
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
